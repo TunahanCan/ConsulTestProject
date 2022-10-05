0781987435a9a52c6548e2a2ca147a515ceff43f
TunahanCan <tunahan.can@hotmail.com.tr>
2022-10-06 00:21:25 +0300

    Add to api gateway config

--- ApiGateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ ApiGateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -3,8 +3,6 @@ package com.example.apigateway.config;
 
 import com.example.apigateway.filter.AuthenticationFilter;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
